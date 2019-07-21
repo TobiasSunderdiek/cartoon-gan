17a80976eb1e0d8354455eec575bc4c7fe2a8a6f
Tobias Sunderdiek <sunderdiek@gmail.com>
2019-07-21 20:39:26 +0200

    make downloader restart-able

--- image_downloader.py
+++ image_downloader.py
@@ -2,9 +2,9 @@ import urllib.request
 import shutil
 import os
 
-PATH_TO_SAFEBOORU_ALL_DATA_CSV = '/Users/ts/Downloads/all_data.csv'
-PATH_TO_STORE_DOWNLOADED_IMAGES = '/Users/ts/Download/safebooru/'
-COUNT_IMAGES_TO_DOWNLOAD = 1000
+PATH_TO_SAFEBOORU_ALL_DATA_CSV = './all_data.csv'
+PATH_TO_STORE_DOWNLOADED_IMAGES = './safebooru/'
+COUNT_IMAGES_TO_DOWNLOAD = 1500
 
 def main():
     if not os.path.exists(PATH_TO_STORE_DOWNLOADED_IMAGES):
@@ -12,19 +12,34 @@ def main():
 
     with open(PATH_TO_SAFEBOORU_ALL_DATA_CSV) as raw_data:
         raw_data.readline() # ignore header line by reading it
-        counter = 1
+        counter = init_counter()
         while counter <= COUNT_IMAGES_TO_DOWNLOAD:
             success = fetch_and_save_image(raw_data.readline())
             if success:
                 counter += 1
                 print('Download #', counter)
 
+def init_counter():
+    if not os.path.exists(PATH_TO_STORE_DOWNLOADED_IMAGES):
+        return 0
+    else:
+        already_existing_no_of_files = len(os.listdir(PATH_TO_STORE_DOWNLOADED_IMAGES))
+        print("Already downloaded files: ", already_existing_no_of_files)
+        return already_existing_no_of_files
+
 def fetch_and_save_image(raw_data_line):
     url = get_image_url(raw_data_line)
     if url.endswith(".jpg"): # download jpg only
         filename_with_path = get_filename_with_path(url)
+        return download_non_already_existing_images(filename_with_path, url)
+    else:
+        return False
+
+def download_non_already_existing_images(filename_with_path, url):
+    if not os.path.exists(filename_with_path):
         return download_image(url, filename_with_path)
     else:
+        print("Skipping file, already exists: ", filename_with_path)
         return False
 
 def get_image_url(raw_data_line):
